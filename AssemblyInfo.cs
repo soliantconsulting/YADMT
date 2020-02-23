4eaf87906f783d2628e1b9332f33f97dc8ed5a66
Brian Engert <lart2150@gmail.com>
2020-02-23 17:58:29 -0600

    added assembly info so exe will have metadata

---
+++ AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+
+    [assembly: AssemblyTitle("DMTHelper")]
+    [assembly: AssemblyDescription("DMTHelper is a tool to help run the filemaker data migration tool")]
+    [assembly: AssemblyConfiguration("")]
+    [assembly: AssemblyCompany("Soliant Consulting, Inc")]
+    [assembly: AssemblyProduct("DMTHelper")]
+    [assembly: AssemblyCopyright("Copyright © 2020 Soliant Consulting, Inc")]
+    [assembly: AssemblyTrademark("")]
+    [assembly: AssemblyCulture("")]
+    [assembly: AssemblyVersion("0.1.0")]
+    [assembly: AssemblyFileVersion("0.1.0")]
